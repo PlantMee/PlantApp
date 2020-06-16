b3cb624ba1efb28e9c420a4f4989a654826295d0
Deepak <deep.jhav@gmail.com>
2020-06-16 14:55:26 +0530

    REALLY AN WESEOME SITE IS DEVELOPING ONE OF THE INITIL FINAL FRONTEND BUILD v6.0.0

--- routes/user_pages.js
+++ routes/user_pages.js
@@ -21,7 +21,6 @@ var parentNav=[];
 	 	}
 	 	return parentNav;
 	});
-
  //HOME PAGE DATA..........................
 ///>>>>>MAIN ROUTE<<<<<<<<<<
 router.get("/",function (req,res) {	
@@ -64,14 +63,21 @@ router.get("/",function (req,res) {
 ////////////////////////////////////
 ///>>>>>ALL PRODUCTS SHOW PAGE<<<<<<<<<<
 router.get("/allProducts",(req,res)=>{
-	Plant.find({},(err,foundPlant)=>{
-		if (err){
-			req.flash("error","SOMETHING WENT WRONG PLEASE RETRY AFTER SOMETIME");
-			res.redirect("back");
-		}else{
-			res.render("userArea/allProducts",{plant:foundPlant});
-		}
-	})
+	Page.find({}).populate("childCategory").exec((err,foundPage)=>{
+	 	if(err){
+	 		req.flash("error","SOMETHING WENT WRONG PLEASE RETRY AFTER SOMETIME");
+			res.redirect("back")
+	 	}else{	
+		 	Plant.find({},(err,foundPlant)=>{
+				if (err){
+					req.flash("error","SOMETHING WENT WRONG PLEASE RETRY AFTER SOMETIME");
+					res.redirect("back");
+				}else{
+					res.render("userArea/allProducts",{page:foundPage,plant:foundPlant});
+				}
+			})		 		 				 		
+	 	}		
+	});		
 })
 /////////////////////////////////////
 //SUBSCRIBE TO NEWS LETTER
@@ -117,21 +123,34 @@ router.post("/plantae/contactUs",(req,res)=>{
 ////HOME PAGE DATA END........................................
 ///>>>>>CATEGORY PAGES<<<<<<<<<<
 router.get("/category/:id",(req,res)=>{
-	Plant.find({category:req.params.id},(err,foundPlant)=>{
-		if (err){
-			req.flash("error","SOMETHING WENT WRONG PLEASE RETRY AFTER SOMETIME");
-			res.redirect("back");
-		}else{
-			res.render("userArea/categoryShowPage",{plant:foundPlant});
-		}
-
-	})
+	Page.find({}).populate("childCategory").exec((err,foundPage)=>{
+	 	if(err){
+	 		req.flash("error","SOMETHING WENT WRONG PLEASE RETRY AFTER SOMETIME");
+			res.redirect("back")
+	 	}else{		 		
+		 	Plant.find({category:req.params.id},(err,foundPlant)=>{
+				if (err){
+					req.flash("error","SOMETHING WENT WRONG PLEASE RETRY AFTER SOMETIME");
+					res.redirect("back");
+				}else{
+					res.render("userArea/categoryShowPage",{page:foundPage,plant:foundPlant});
+				}
+			})		 		 				 		
+	 	}		
+	});		
 })
 ///>>>>>PRODUCT PAGES<<<<<<<<<<
 router.get("/product/:id",(req,res)=>{
-	Plant.findOne({_id:req.params.id}).populate("category").exec((err,foundPlant)=>{		
-		res.render("userArea/productShowPage",{plant:foundPlant});
-	})	
+	Page.find({}).populate("childCategory").exec((err,foundPage)=>{
+	 	if(err){
+	 		req.flash("error","SOMETHING WENT WRONG PLEASE RETRY AFTER SOMETIME");
+			res.redirect("back")
+	 	}else{		 		
+		 	Plant.findOne({_id:req.params.id}).populate("category").exec((err,foundPlant)=>{		
+				res.render("userArea/productShowPage",{page:foundPage,plant:foundPlant});
+			})	 		 				 		
+	 	}		
+	});	
 })
 /////////////////////////////////////////////////////////
 ///>>>>>LOGIN<<<<<<<<<<
@@ -201,59 +220,18 @@ router.get("/logout",(req,res)=>{
 })
 //...................................................
 
-//ROUTE FOR TO CART PAGE
-router.get("/cart",Validation.isLoggedIn,(req,res)=>{
-	User.findById(req.user._id,(err,foundUser)=>{
-		// var cartData= foundUser.cart;
-		res.render("userArea/productCart",{cart:foundUser.cart});	
-	})
-	
-})
-//......................
-//Add an item to cart route
-router.get("/addToCart/:id",Validation.isLoggedIn,(req,res)=>{
-	User.findOne(req.user._id,(err,foundUser)=>{
-		if(err){
-			req.flash("error",err.message);
-			return res.redirect("back");
-		}else{			
-			Plant.findOne({_id:req.params.id},(err,foundPlant)=>{
-				if(err){
-					req.flash("error",err.message);
-					return res.redirect("back");
-				}else{
-					req.flash("success","SUCCESSFULLY ADDED "+ foundPlant.name +" TO CART");					
-					// var listCart=[];
-					// foundUser.cart.forEach(function(data){
-					// 	listCart.push(data._id);
-					// })
-					// var checker =foundPlant._id;
-					// console.log(typeof checker)
-					// for(var i =0 ;i<listCart.length;i++){
-					// 	console.log(listCart[i]);
-					// }
-					//There is a damn bug in the whole system as we can add same item multiple times
-					var objectCart ={
-						plantId:foundPlant._id,
-						plantName:foundPlant.name,
-						plantImage:foundPlant.image,
-						plantDesc:foundPlant.description,
-						price:foundPlant.price,
-						quantity:1
-					};
-
-						foundUser.cart.push(objectCart);	
-						foundUser.save();
-						res.redirect("back");
-				}
-			})		
-		}
-	})
-});
-//............................
 
 
-
+function data(){
+     Page.find({}).populate("childCategory").exec((err,foundPage)=>{
+	 	if(err){
+	 		return err;
+	 	}else{
+	 		return foundPage;
+	 		
+	 	}	 
+	});
+ }
 //......./......../......
 //EXPORTS
 module.exports=router;
